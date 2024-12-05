30c05c75d39fa7d38bc7f53079a5f25f2c470d48
Mohamed BOUCHERBA <mboucherba31@gmail.com>
2024-12-05 10:05:33 +0100

    search aircraft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AircraftsComponent } from './components/aircrafts/aircrafts.component';
 import { AircraftsNavbarComponent } from './components/aircrafts/aircrafts-navbar/aircrafts-navbar.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { AircraftsNavbarComponent } from './components/aircrafts/aircrafts-navba
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({airBusState : AircraftsReducer}), //spécifie le reducer
+    EffectsModule.forRoot([AircraftsEffects]), //spécifie les effects
+    StoreDevtoolsModule.instrument() //en l'activant ici, à chaque action de NGRX, le plugin redux permet l'analyse du state durant le dev
   ],
   providers: [],
   bootstrap: [AppComponent]
